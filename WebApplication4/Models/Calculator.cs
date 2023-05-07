ce9a44f497fab753bce57fd041fc1ebb6bae651e
dlabaja <dlabaja.jan@seznam.cz>
2023-05-07 13:33:29 +0200

    Initial commit

---
+++ WebApplication4/Models/Calculator.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+using System.Web.Helpers;
+
+public class Calculator
+{
+	private string result = "";
+	private Dictionary<char, Func<int, int, int>> strPairToResult = new Dictionary<char, Func<int, int, int>>()
+	{
+		{'-', (int num1, int num2) => num1-num2 },
+		{'+', (int num1, int num2) => num1+num2 }
+	};
+
+	public string Calculate(string query)
+	{
+		result = query;
+		try
+		{
+			CalculateStringToResult('-');
+			CalculateStringToResult('+');
+		}
+		catch { }
+		return result;
+	}
+
+	private void CalculateStringToResult(char symbol)
+	{
+		var match = new Regex($@"(\d+)\{symbol}(\d+)").Match(result); // finds pair with a given symbol
+		var pair = match.ToString().Split(symbol);
+		var funcResult = 0;
+
+		if (!match.Success) return;
+		if (match.Index >= 1 && result[match.Index - 1] == '-')
+		{
+			funcResult = strPairToResult[symbol](int.Parse($"-{pair[0]}"), int.Parse(pair[1]));
+			result = result.Replace("-" + match.Value, funcResult.ToString());
+			return;
+		}
+
+		funcResult = strPairToResult[symbol](int.Parse(pair[0]), int.Parse(pair[1]));
+		result = result.Replace(match.Value, funcResult.ToString());
+	}
+}
---
+++ WebApplication4/Scripts/site.js
@@ -0,0 +1,49 @@
+﻿let output = null;
+const symbols = "+-"
+
+function loadDoc(){
+    output = document.getElementById("output")
+}
+
+function writeToTextBox(val) {
+    const symbols = "+-"
+    if (output.value === null) {
+        output.value += val
+        return
+    }
+    if (symbols.includes(output.value[output.value.length - 1]) && symbols.includes(val)) return //check for duplicate symbols
+    output.value += val
+}
+
+function writeZero(){
+    if (symbols.includes(getLastChar()) || getLastChar() === null) return
+    writeToTextBox(0);
+}
+
+function writeSymbol(){
+    
+}
+
+function getLastChar() {
+    if (output.value.length < 1) return null
+    return output.value[output.value.length - 1]
+}
+    
+function sendToServer() {
+    fetch('https://localhost:44381/home/calculate?val=' + encodeURIComponent(output.value))
+        .then(response => response.text())
+        .then(data => {
+            console.log(data)
+            output.value = data
+        })
+        .catch(error => console.log(error));
+}
+
+function delete_char(){
+    if (output.value.length < 1) return
+    output.value = output.value.slice(0, -1);
+}
+
+function clear_output(){
+    output = ""
+}
